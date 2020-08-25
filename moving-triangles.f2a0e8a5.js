57f329ef7f46a72183727ba5a551a31faee75035
metzlr <reedmg15@gmail.com>
2020-08-24 22:12:40 -0700

    Deploying to Github Pages

--- moving-triangles.f2a0e8a5.js
+++ moving-triangles.f2a0e8a5.js
@@ -998,7 +998,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33131" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40153" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
